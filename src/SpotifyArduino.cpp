c366aebb8e4bba88497edc1971ee5ac8a951d4a8
Marcel Stör <marcelstoer@users.noreply.github.com>
2025-06-13 07:23:38 +0200

    Fix SSL sending empty body
    Fixes #69

--- src/SpotifyArduino.cpp
+++ src/SpotifyArduino.cpp
@@ -83,7 +83,14 @@ int SpotifyArduino::makeRequestWithBody(const char *type, const char *command, c
 
     client->println();
 
-    client->print(body);
+    if (strlen(body) == 0)
+    {
+        client->println();
+    }
+    else
+    {
+        client->print(body);
+    }
 
     if (client->println() == 0)
     {
